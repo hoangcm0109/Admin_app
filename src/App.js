b5f0a6c67d2f6954d4ede0f0af7fc21552f57944
Minh Hoàng <chuminhhoang19@gmail.com>
2022-01-16 17:02:58 +0700

    sidebar and base layout

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="orders" element={<Blank />} />
-          <Route path="product" element={<Blank />} />
+          <Route path="products" element={<Blank />} />
           <Route path="customers" element={<Blank />} />
           <Route path="stats" element={<Blank />} />
+          <Route path="settings" element={<Blank />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './sidebar.scss'
+import { Link, useLocation } from 'react-router-dom'
+import { images } from '../../constants'
+import sidebarNav from '../../configs/sidebarNav'
 
 const Sidebar = () => {
+
+    const [activeIndex, setActiveIndex] = useState(0)
+
+
     return (
-        <div>
-            Sidebar
+        <div className='sidebar'>
+            <div className="sidebar_logo">
+                <img src={images.logo} alt="" />
+                <div className="sidebar-close">
+                    <i className='bx bx-x'></i>
+                </div>
+            </div>
+            <div className="sidebar_menu">
+                {
+                    sidebarNav.map((nav, index) => (
+                        <Link to={nav.link} key={index} className={`sidebar_menu_item ${activeIndex === index && 'active'}`}>
+                            <div className="sidebar_menu_item_icon">
+                                {nav.icon}
+                            </div>
+                            <div className="sidebar_menu_item_txt">
+                                {nav.text}
+                            </div>
+                        </Link>
+                    ))
+                }
+                <div className="sidebar_menu_item">
+                    <div className="sidebar_menu_item_icon">
+                        <i class='bx bx-log-out' ></i>
+                    </div>
+                    <div className="sidebar_menu_item_txt">
+                        Logout
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
