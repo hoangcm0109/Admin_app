608704bf387296a0cb198417f77f4faa8ddeed82
Minh Hoàng <chuminhhoang19@gmail.com>
2022-01-19 10:21:34 +0700

    layout dashboard main and chart

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Box from '../components/box/Box'
+import DashboardWrapper, { DashboardWrapperMain, DashboardWrapperRight } from '../components/dashboard-wrapper/DashboardWrapper'
+import SummaryBox, { SummaryBoxSpecial } from '../components/summary-box/SummaryBox'
+import { data } from '../constants'
 
 const Dashboard = () => {
     return (
         <div>
-            Dashboard
+            <DashboardWrapper>
+                <DashboardWrapperMain>
+                    <div className="row">
+                        <div className="col-8 col-md-12">
+                            <div className="row">
+                                {
+                                    data.summary.map((item, index) => (
+                                        <div key={index} className="col-6 col-md-6 col-md-12 mb">
+                                            <SummaryBox item={item} />
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <div className="col-4 hide-md">
+                            <SummaryBoxSpecial item={data.revenueSummary} />
+                        </div>
+                    </div>
+                </DashboardWrapperMain>
+                <DashboardWrapperRight>
+                    DashboardWrapperRight
+                </DashboardWrapperRight>
+            </DashboardWrapper>
         </div>
     )
 }
