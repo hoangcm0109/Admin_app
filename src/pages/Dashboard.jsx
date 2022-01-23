c3a7ff190fd6d1a4263ae1bea47231894b22ac07
Minh Hoàng <chuminhhoang19@gmail.com>
2022-01-23 15:16:18 +0700

    update chart dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Bar } from 'react-chartjs-2'
 import Box from '../components/box/Box'
 import DashboardWrapper, { DashboardWrapperMain, DashboardWrapperRight } from '../components/dashboard-wrapper/DashboardWrapper'
 import SummaryBox, { SummaryBoxSpecial } from '../components/summary-box/SummaryBox'
-import { data } from '../constants'
+import { colors, data } from '../constants'
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
 
 const Dashboard = () => {
     return (
@@ -25,9 +47,17 @@ const Dashboard = () => {
                             <SummaryBoxSpecial item={data.revenueSummary} />
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <Box>
+                                <RevenueByMothsChart />
+                            </Box>
+                        </div>
+                    </div>
                 </DashboardWrapperMain>
                 <DashboardWrapperRight>
-                    DashboardWrapperRight
+                    <div className="title mb">Overall</div>
+                    <div className="title mb">Revenue by Channel</div>
                 </DashboardWrapperRight>
             </DashboardWrapper>
         </div>
@@ -35,3 +65,60 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
+const RevenueByMothsChart = () => {
+    const chartOptions =  {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            xAses: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            yAses: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false
+            }
+        },
+        elements: {
+            bar: {
+                backgroundColor: colors.orange,
+                borderRadius: 20,
+                borderSkiped: 'bottom'
+            }
+        }
+    }
+
+    const chartData = {
+        labels: data.revenueByMonths.labels,
+        datasets: [
+            {
+                labels: 'Revenue',
+                data: data.revenueByMonths.data
+            }
+        ]
+    }
+
+    return (
+        <>
+            <div className="title">
+                Revenue by months
+            </div>
+            <div>
+                <Bar options={chartOptions} data={chartData} height={`300px`}/>
+            </div>
+        </>
+    )
+}
